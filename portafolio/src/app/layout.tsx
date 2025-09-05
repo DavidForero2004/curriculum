422bcd91af401e716ee17435a430f79854857797
DavidForero2004 <davidforeromoreno2016@gmail.com>
2025-09-04 22:16:24 -0500

    anuncios

--- portafolio/src/app/layout.tsx
+++ portafolio/src/app/layout.tsx
@@ -1,17 +1,18 @@
 import "../styles/global.css";
 
-import Script from "next/script";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="es">
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8163540274916215"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        ></script>
       </head>
       <body>{children}</body>
     </html>

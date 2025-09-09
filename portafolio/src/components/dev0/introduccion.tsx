9da59b22da6b20ae276788d6378d9e89fb8b6a63
DavidForero2004 <davidforeromoreno2016@gmail.com>
2025-09-09 17:49:44 -0500

    resposive

--- portafolio/src/components/dev0/introduccion.tsx
+++ portafolio/src/components/dev0/introduccion.tsx
@@ -1,23 +1,52 @@
-import { playfairDisplay, poppins } from "../../../public/fonts/font";
-export default function Introduccion() {
+import { playfairDisplay, poppins, inter } from "../../../public/fonts/font";
+import { useState } from "react";
+/*import HamburgerMenu from "./hamburgerMenu";*/
+
+type NavsProps = {
+  showNav: boolean;
+};
+
+export default function Introduccion({showNav}: NavsProps) {
+  const scrollToSection = (id: string) => {
+    document.getElementById(id)?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const [activeLabel, setActiveLabel] = useState<string | null>(null);
+
+  const david = activeLabel;
+
+
+
   return (
     <section className="banner-intro fade-section" id="intro-section">
-            <div className="banner-container">
-              <div className="banner-title">
-                <p id="name" className={playfairDisplay.className}>
-                  David Forero
-                </p>
-              </div>
-              <div className="banner-occupation">
-                <p id="occupation" className={poppins.className}>
-                  Desarrollador de software
-                </p>
-              </div>
-            </div>
-            <div className="scroll-down">
-              <p>Ver más</p>
-              <p>↓</p>
-            </div>
-          </section>
+      {/*<HamburgerMenu />*/ david}
+      <div className="banner-container">
+        <div className="banner-title">
+          <p id="name" className={playfairDisplay.className}>
+            David Forero
+          </p>
+        </div>
+        <div className="banner-occupation">
+          <p id="occupation" className={poppins.className}>
+            Desarrollador de software
+          </p>
+        </div>
+      </div>
+      <div className={`scroll-down ${showNav ? "visible" : ""}`} >
+        <a
+          className={inter.className}
+          onClick={(e) => {
+            e.preventDefault();
+            scrollToSection("about-section");
+             setActiveLabel("intro");
+          }}
+        >
+          <p>Ver más</p>
+          <p>↓</p>
+        </a>
+      </div>
+    </section>
   );
-}   
+}

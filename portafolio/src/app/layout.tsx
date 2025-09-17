d418069010b5ff8f51aa44646da4d56352278d83
DavidForero2004 <davidforeromoreno2016@gmail.com>
2025-09-17 10:13:10 -0500

    script ads

--- portafolio/src/app/layout.tsx
+++ portafolio/src/app/layout.tsx
@@ -13,6 +13,16 @@ export default function RootLayout({
           name="google-adsense-account"
           content="ca-pub-8163540274916215"
         ></meta>
+        <script
+          async
+          custom-element="amp-auto-ads"
+          src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
+        ></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8163540274916215"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>
         {children}
